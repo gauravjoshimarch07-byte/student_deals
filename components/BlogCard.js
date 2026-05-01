df61d1bbe01c13b29f49516f08fe657fb7e2eb00
Gaurav Joshi <gauravjoshimarch07@gmail.com>
2026-05-01 19:28:27 +0530

    fixed image layout

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -17,8 +17,8 @@ export default function BlogCard({ post, featured = false }) {
     <Link href={`/blog/${post.slug}`} className="group block">
       <article className={`card overflow-hidden h-full flex flex-col ${featured ? 'md:flex-row' : ''}`}>
         {/* Thumbnail */}
-        <div className={`relative overflow-hidden bg-cream-warm ${featured ? 'md:w-2/5 md:flex-shrink-0' : ''}`}>
-         <div className={`${featured ? 'h-52 md:h-52' : 'h-48'} relative`}>
+       <div className={`relative overflow-hidden bg-cream-warm ${featured ? 'md:w-2/5 md:flex-shrink-0 md:h-auto' : ''}`}>
+         <div className={`${featured ? 'h-52 md:h-full' : 'h-48'} relative`}>
             <Image
               src={post.thumbnail || 'https://images.unsplash.com/photo-1512820790803-83ca734da794?w=800&q=75'}
               alt={post.title}
